a3eda33def14627d6fbdb15e50b69b53c35adfb5
Ilia Shumski <whalterwhite23@gmail.com>
2021-07-09 14:38:58 +0300

    Add footer navigation

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import MovieIcon from '@material-ui/icons/Movie';
+import TvIcon from '@material-ui/icons/Tv';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles({
+  root: {
+    width: 100 + '%',
+    position: 'fixed',
+    bottom: 0,
+    backgroundColor: '#2d313a',
+    zIndex: 100,
+  },
+});
+
+export default function SimpleBottomNavigation() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction
+        label="Trending"
+        icon={<WhatshotIcon />}
+        style={{ color: '#ffffff' }}
+      />
+
+      <BottomNavigationAction
+        label="Movies"
+        icon={<MovieIcon />}
+        style={{ color: '#ffffff' }} />
+
+      <BottomNavigationAction
+        label="TV Series"
+        icon={<TvIcon />}
+        style={{ color: '#ffffff' }} />
+
+      <BottomNavigationAction
+        label="Search"
+        icon={<SearchIcon />}
+        style={{ color: '#ffffff' }} />
+    </BottomNavigation>
+  );
+}
